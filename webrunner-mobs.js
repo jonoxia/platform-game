87a0532dd26988abb0f1f17782510c8b83d8bfdb
Jono X <ebjono@gmail.com>
2011-11-06 16:46:51 +0000

    Added octogoblin

--- webrunner-mobs.js
+++ webrunner-mobs.js
@@ -283,6 +283,64 @@ Enemy.prototype = {
 Enemy.prototype.__proto__ = new Mob();
 ConstructorRegistry.register(Enemy);
 
+function Octogoblin() {
+  this.mobInit("octogoblin.png", false);
+  this.direction = "left";
+}
+Octogoblin.prototype = {
+  type: "octogoblin",
+  width: 73,
+  height: 62,
+
+  roam: function() {
+    if (this.direction == "left" && 
+	(TheWorld.touchingPlatform(this, "left") != null)) {
+	this.direction = "right";
+    } else if (this.direction == "right" &&
+       (TheWorld.touchingPlatform(this, "right") != null)) {
+	this.direction = "left";
+    }
+
+    // change direction when you hit a ledge/edge of platform:
+    var underMe = TheWorld.touchingPlatform(this, "bottom");
+    if (underMe) {
+	if (this.direction == "left" &&
+	    (this.left - underMe.left < 10) ) {
+	    this.direction = "right";
+	}
+	if (this.direction == "right" &&
+	    (underMe.right - this.right < 10) ) {
+	    this.direction = "left";
+	}
+    }
+
+    if (this.direction == "left") {
+	this.goLeft();
+    } else if (this.direction == "right") {
+	this.goRight();
+    }
+
+  },
+  onMobTouch: function(mob, intercept) {
+    // If touch player, hurt player if touching from the
+    // side; kill enemy if player jumps on its head.
+    if (mob.type == "player") {
+	if (intercept.side == "top") {
+	    this.die();
+	    // TODO death animation?
+	    mob.vy = -10; // bounce
+	    playSfx("crunch-sfx");
+        } else {
+	    mob.die();
+	}
+    }
+    // TODO return true or false? stop mob at intercept?
+  }
+};
+Octogoblin.prototype.__proto__ = new Mob();
+ConstructorRegistry.register(Octogoblin);
+
+
 function Dino() {
   this.mobInit("running_chaos_frames.png", true);
   this.direction = "left";
